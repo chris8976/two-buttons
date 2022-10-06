4f7ca65b6df60cace63e59aabb2c42775a04d9fc
Christopher Dominguez <Neymar15216@gmail.com>
2022-10-06 14:08:49 -0400

    jjj

--- two buttons/ViewController.swift
+++ two buttons/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        Label.text = textField.text
+    }
+    
+    
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        Label.text = ""
+    }
 }
 
